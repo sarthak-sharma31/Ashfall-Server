91353f849e03156a972f5c632cff4d96513265da
Sarthak Sharma <sarthak312005@gmail.com>
2026-06-05 12:07:15 +0530

    added db

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/propocalypse");
-
-        console.log("✅ MongoDB Connected");
-    }
-    catch (err) {
-        console.log(err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
+  }
 }
